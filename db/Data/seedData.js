57687697f3415d0f7b16e447cd2134403a5df3b3
kshieldsDTS <escanor05@gmail.com>
2022-01-25 15:24:04 -0500

    add settings.json seed data. add logic to seed settings in seedData.js

--- db/Data/seedData.js
+++ db/Data/seedData.js
@@ -1,7 +1,11 @@
 // Import Model(s)
 const Task = require('../Models/Task');
+const Setting = require('../Models/Setting')
+const Message = require('../Models/Setting')
 // Import Seed(s)
 const taskSeed = require('../Data/tasks.json');
+const settingSeed = require('../Data/settings.json')
+// const messageSeed = require('../Data/mesages.json')
 
 Task.deleteMany({})
     .then(() => {
@@ -10,6 +14,15 @@ Task.deleteMany({})
     })
     .then((tasks) => {
         console.log('Created tasks', tasks);
+        Setting.deleteMany({})
+    })
+    .then(() => {
+        console.log('Deleted Settings!');
+        return Setting.insertMany(settingSeed)
+    })
+    .then((settings) => {
+        console.log('Created settings', settings);
+        // Message.deleteMany({})
     })
     .catch(console.error)
     .finally(() => process.exit())
